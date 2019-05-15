6f292ce7078bff80cd929e02cda4b67e47a0b251
Soumava <soumava@soumavas-mbp.in.ibm.com>
2019-05-15 17:41:11 +0530

    doc2vec embeddings

---
+++ code/doc2vec_embeddings.py
@@ -0,0 +1,135 @@
+from gensim.models import KeyedVectors
+from gensim.models.doc2vec import TaggedDocument, Doc2Vec
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import FunctionTransformer, MultiLabelBinarizer
+
+from nltk import sent_tokenize
+from nltk import pos_tag
+from nltk import map_tag
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+
+import pandas as pd
+import numpy as np
+import torch
+import json
+import re
+import csv
+import pickle
+from tqdm import tqdm
+
+def clean_text(text):
+    # remove a string like {{plot}}
+    text = re.sub("\s*{{\w*}}\s*", "", text)
+    # remove backslash-apostrophe 
+    text = re.sub("\'", "", text)
+    
+    text = text.lower().replace('\n', ' ').replace('\t', ' ').replace('\xa0',' ') #get rid of problem chars
+    
+    text = ' '.join(text.split())
+    
+    return text
+
+def doc2vec(data_df):
+    data = []
+    print("Building TaggedDocuments")
+    total = len(data_df[['movie_id', 'clean_plot']].as_matrix().tolist())
+    processed = 0
+    for x in data_df[['movie_id', 'clean_plot']].as_matrix().tolist():
+        label = ["_".join(x[0].split())]
+        words = []
+        sentences = sent_tokenize(x[1])
+        for s in sentences:
+            words.extend([x for x in word_tokenize(s)])
+        doc = TaggedDocument(words, label)
+        data.append(doc)
+
+        processed += 1
+        if processed % 10000 == 0:
+            print(processed, "/", total)
+
+    print('Done!')
+
+    model = Doc2Vec(min_count=1, window=10, size=300, sample=1e-5, negative=5, workers=2, epochs=20, min_alpha=0.00025)
+    
+    print("Building Vocabulary")
+    model.build_vocab(data)
+    print('Done!')
+
+    print("Training starts")
+    
+    model.train(documents=data, total_examples=model.corpus_count, epochs=model.epochs)
+
+    print('\nTraining complete')
+    
+    print('\nBuilding doc2vec vectors')
+    # Build doc2vec vectors
+    x_data = []
+    genres = data_df['genre_new'].as_matrix().tolist()
+    binarizer = MultiLabelBinarizer()
+    y_data = binarizer.fit_transform(genres)
+    ids = data_df[['movie_id']].as_matrix().tolist()
+    for i in range(len(ids)):
+        movie_id = ids[i][0]
+        label = "_".join(movie_id.split())
+        x_data.append(model.docvecs[label])
+
+    return np.array(x_data), y_data
+
+metadata = pd.read_csv("../data/movie.metadata.tsv", sep = '\t', header = None)
+metadata.columns = ["movie_id",1,"movie_name",3,4,5,6,7,"genre"]
+
+plots = []
+with open("../data/plot_summaries.txt", 'r') as f:
+    reader = csv.reader(f, dialect='excel-tab') 
+    for row in tqdm(reader):
+        plots.append(row)
+
+movie_id = []
+plot = []
+
+# extract movie Ids and plot summaries
+for i in tqdm(plots):
+    movie_id.append(i[0])
+    plot.append(i[1])
+
+# create dataframe
+movies = pd.DataFrame({'movie_id': movie_id, 'plot': plot})
+
+# change datatype of 'movie_id'
+metadata['movie_id'] = metadata['movie_id'].astype(str)
+
+# merge meta with movies
+movies = pd.merge(movies, metadata[['movie_id', 'movie_name', 'genre']], on = 'movie_id')
+
+genres = [] 
+
+# extract genres
+for i in movies['genre']: 
+    genres.append(list(json.loads(i).values())) 
+
+# add to 'movies' dataframe  
+movies['genre_new'] = genres
+
+movies_new = movies[~(movies['genre_new'].str.len() == 0)]
+
+movies_new['clean_plot'] = movies_new['plot'].apply(lambda x: clean_text(x))
+
+x_data, y_data = doc2vec(movies_new)
+
+print('Saving data to disk')
+
+with open('/dccstor/cmv/MovieSummaries/embeddings/doc2vec_embeddings.pkl', 'wb') as handle:
+    pickle.dump((x_data, y_data), handle)
+
+print('Finished!')
---
+++ code/infersent_embeddings.py
@@ -0,0 +1,129 @@
+import pandas as pd
+import numpy as np
+import torch
+import json
+import nltk
+import re
+import csv
+from tqdm import tqdm
+from nltk.tokenize import sent_tokenize
+
+import InferSent
+from InferSent.models import InferSent
+
+metadata = pd.read_csv("../data/movie.metadata.tsv", sep = '\t', header = None)
+metadata.columns = ["movie_id",1,"movie_name",3,4,5,6,7,"genre"]
+
+plots = []
+with open("../data/plot_summaries.txt", 'r') as f:
+    reader = csv.reader(f, dialect='excel-tab') 
+    for row in tqdm(reader):
+        plots.append(row)
+
+movie_id = []
+plot = []
+
+# extract movie Ids and plot summaries
+for i in tqdm(plots):
+    movie_id.append(i[0])
+    plot.append(i[1])
+
+# create dataframe
+movies = pd.DataFrame({'movie_id': movie_id, 'plot': plot})
+
+# change datatype of 'movie_id'
+metadata['movie_id'] = metadata['movie_id'].astype(str)
+
+# merge meta with movies
+movies = pd.merge(movies, metadata[['movie_id', 'movie_name', 'genre']], on = 'movie_id')
+
+genres = [] 
+
+# extract genres
+for i in movies['genre']: 
+    genres.append(list(json.loads(i).values())) 
+
+# add to 'movies' dataframe  
+movies['genre_new'] = genres
+
+movies_new = movies[~(movies['genre_new'].str.len() == 0)]
+
+def clean_text(text):
+    # remove a string like {{plot}}
+    text = re.sub("\s*{{\w*}}\s*", "", text)
+    # remove backslash-apostrophe 
+    text = re.sub("\'", "", text)
+    return text
+
+movies_new['clean_plot'] = movies_new['plot'].apply(lambda x: clean_text(x))
+
+print('Creating vocabulary for dataset....')
+
+new_vocabulary = []
+
+for movie_id in movies_new['movie_id']:
+    
+    plot_sr = movies_new[movies_new['movie_id']==movie_id]['clean_plot']
+    
+    for str_obj in plot_sr:
+        plot=str_obj
+    
+    sentence_list = sent_tokenize(plot)
+
+    new_vocabulary = new_vocabulary + sentence_list
+
+print('\nVocab creation done!')
+
+V = 2
+MODEL_PATH = '/u/soupaul5/All_Data/genre_prediction/pretrained_models/infersent%s.pkl' % V
+params_model = {'bsize': 128, 'word_emb_dim': 300, 'enc_lstm_dim': 2048,
+				'pool_type': 'max', 'dpout_model': 0.0, 'version': V}
+
+model = InferSent(params_model)
+model.load_state_dict(torch.load(MODEL_PATH))
+model = model.cuda()
+
+W2V_PATH = '/u/soupaul5/All_Data/genre_prediction/fastText/crawl-300d-2M.vec'
+model.set_w2v_path(W2V_PATH)
+
+model.build_vocab_k_words(K=500000)
+
+print('Updating Vocab....')
+model.update_vocab(new_vocabulary, tokenize=True)
+print('Vocab updated!')
+
+embedding_dict = {}
+
+count = 0
+
+print('Embedding creation begins...')
+
+for movie_id in movies_new['movie_id']:
+    
+    plot_sr = movies_new[movies_new['movie_id']==movie_id]['clean_plot']
+    
+    for str_obj in plot_sr:
+        plot=str_obj
+    
+    sentence_list = sent_tokenize(plot)
+
+    embedding_array = model.encode(sentence_list, tokenize=True)
+
+    embedding_dict[movie_id] = embedding_array
+
+    count += 1
+
+    # if count==10:
+    # 	break
+
+    if count%1000==0:
+    	print('PLOTS processed : {}/{}'.format(count, movies_new.shape[0]))
+
+print('DICTIONARY LENGTH: {}'.format(len(embedding_dict)))
+
+# Save to disk
+print('\nSaving entire dictionary to disk...')
+np.save("/dccstor/cmv/MovieSummaries/embeddings/Infersent_embeddings.npy", embedding_dict)
+print('Finished!')
+
+
