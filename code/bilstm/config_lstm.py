632cc868aade092a747e62dc79c86a0e3b95029d
Soumava <soumava@soumavas-mbp.in.ibm.com>
2019-05-21 11:10:53 +0530

    debug

--- code/bilstm/config_lstm.py
+++ code/bilstm/config_lstm.py
@@ -1,6 +1,7 @@
 METADATA_PATH = "/dccstor/cmv/MovieSummaries/movie.metadata.tsv"
 PLOT_SUMMARIES_PATH = "/dccstor/cmv/MovieSummaries/plot_summaries.txt"
-BATCH_SIZE = 16
+TRAIN_BATCH_SIZE = 16
+VAL_BATCH_SIZE = 1
 LEARNING_RATE = 0.001
 DROPOUT_RATE = 0.2
 NUM_EPOCHS = 100
