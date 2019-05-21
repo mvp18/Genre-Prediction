26fe546e589863a1604c3c65aaf51e5d4cf8c21d
Soumava <soumava@Soumavas-MacBook-Pro.local>
2019-05-22 03:23:10 +0530

    bug fix

--- code/bilstm/loss_func.py
+++ code/bilstm/loss_func.py
@@ -7,16 +7,16 @@ def weighted_loss(wout): # assuming weight size is (output_size, 2)
 	
 	def w_binary_crossentropy(y_true, y_pred):
 		
-			ce = K.binary_crossentropy(y_true, y_pred) # (None, output_size)
+		ce = K.binary_crossentropy(y_true, y_pred) # (None, output_size)
 
-			batch_size = tf.shape(ce)[0]
+		batch_size = tf.shape(ce)[0]
 
-			wout = np.repeat(np.expand_dims(wout, axis=0), batch_size, axis=0)
+		class_wt = np.repeat(np.expand_dims(wout, axis=0), batch_size, axis=0)
 
-			weight = y_true*wout[:, :, 0] + (1 - y_true)*wout[:, :, 1]  # (None, output_size)
-			
-			loss = weight*ce # (None, output_size)
-			
-			return K.mean(loss, axis=None)	# mean loss value
+		weight = y_true*class_wt[:, :, 0] + (1 - y_true)*class_wt[:, :, 1]  # (None, output_size)
+
+		loss = weight*ce # (None, output_size)
+
+		return K.mean(loss, axis=None)	# mean loss value
 	
 	return w_binary_crossentropy
