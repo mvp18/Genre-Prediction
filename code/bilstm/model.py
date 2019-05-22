927acbaeed6eba0049ee18900823091cef50cf05
Soumava <soumava@soumavas-mbp.in.ibm.com>
2019-05-22 16:24:50 +0530

    cuda lstms

--- code/bilstm/model.py
+++ code/bilstm/model.py
@@ -18,13 +18,13 @@ def BiLSTM(num_classes, reg, reg_wt):
     else:
         
         model.add(Bidirectional(CuDNNLSTM(1024, return_sequences=True, kernel_regularizer=regularizers.l1_l2(reg_wt), recurrent_regularizer=regularizers.l1_l2(reg_wt),
-                                                kernel_initializer='he_normal', recurrent_initializer='he_normal'), input_shape=(None, data_dim)))
+                                                kernel_initializer='he_normal', recurrent_initializer='he_normal')))
         
         model.add(Bidirectional(CuDNNLSTM(512, return_sequences=True, kernel_regularizer=regularizers.l1_l2(reg_wt), recurrent_regularizer=regularizers.l1_l2(reg_wt),
-                                                kernel_initializer='he_normal', recurrent_initializer='he_normal'), input_shape=(None, data_dim)))
+                                                kernel_initializer='he_normal', recurrent_initializer='he_normal')))
 
-        model.add(Bidirectional(CuDNNLSTM(256, return_sequences=True, kernel_regularizer=regularizers.l1_l2(reg_wt), recurrent_regularizer=regularizers.l1_l2(reg_wt),
-                                                kernel_initializer='he_normal', recurrent_initializer='he_normal'), input_shape=(None, data_dim)))
+        model.add(Bidirectional(CuDNNLSTM(256, return_sequences=False, kernel_regularizer=regularizers.l1_l2(reg_wt), recurrent_regularizer=regularizers.l1_l2(reg_wt),
+                                                kernel_initializer='he_normal', recurrent_initializer='he_normal')))
 
     
     model.add(Dropout(DROPOUT_RATE, seed=1))
